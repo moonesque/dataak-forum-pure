375bf54a285aa2339955f1b98e5f6562f0de5ae2
Mahan <mahan.abbuspour@gmail.com>
2020-03-13 19:02:21 +0330

    Add config

--- config/config.py
+++ config/config.py
@@ -7,7 +7,6 @@ CONNECTION_STRING = "{drivername}://{user}:{passwd}@{host}:{port}/{db_name}?char
     port="3306",
     db_name="dataak_forum_pure",
 )
-
 # forum links
 forum_url = 'http://forum.dataak.com/index.php'
 login_url = 'http://forum.dataak.com/member.php'
@@ -16,3 +15,12 @@ host = 'forum.dataak.com'
 # user credentials
 forum_username = 'mahan'
 forum_password = '@123456'
+
+# request headers
+headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate",
+            "Host": host,
+}
+
