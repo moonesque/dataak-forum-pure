2fb4b7ff17170a1d858cc5bd28539508f75aa767
Mahan <mahan.abbuspour@gmail.com>
2020-03-13 18:15:49 +0330

    Add config

---
+++ config/config.py
@@ -0,0 +1,18 @@
+# Database credentials
+CONNECTION_STRING = "{drivername}://{user}:{passwd}@{host}:{port}/{db_name}?charset=utf8".format(
+    drivername="mysql",
+    user="root",
+    passwd="oberyn martel",
+    host="localhost",
+    port="3306",
+    db_name="dataak_forum_pure",
+)
+
+# forum links
+forum_url = 'http://forum.dataak.com/index.php'
+login_url = 'http://forum.dataak.com/member.php'
+host = 'forum.dataak.com'
+
+# user credentials
+forum_username = 'mahan'
+forum_password = '@123456'
